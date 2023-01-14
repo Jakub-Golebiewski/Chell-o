16182a5a37ab028492714baa88098cbbd66e1ff0
Jakub Gołębiewski <116796089+Jakub-Golebiewski@users.noreply.github.com>
2023-01-14 01:10:53 +0100

    Wszystko w klasach i baza startowa
    - Wszystko jest zamknięte w klasach
    - Jak nie ma bazy danych lub jest uszkodzona to tworzona jest domyślna

--- chello.py
+++ chello.py
@@ -1,117 +1,110 @@
-## Projekt zbudowany na bazie python 3.7  connector  8.0.31
-
-#Problemy do rozwiązania na później:
-# Jak zaznaczasz hasło to się pokazuje
-
-import mysql.connector #connector musi być zainstalowany
-from mysql.connector import Error
+import mysql.connector
 import tkinter
 import customtkinter
 
+class MySQLConnection:
+    def __init__(self, host, port, database, user, password):
+        self.host = host
+        self.port = port
+        self.database = database
+        self.user = user
+        self.password = password
+        self.connection = None
+        self.cursor = None
+
+    def connect(self):
+        try:
+            self.connection = mysql.connector.connect(host=self.host,
+                                                     port=self.port,
+                                                     database=self.database,
+                                                     user=self.user,
+                                                     password=self.password)
+            if self.connection.is_connected():
+                db_Info = self.connection.get_server_info()
+                print("Connected to MySQL Server version ", db_Info)
+                self.cursor = self.connection.cursor(buffered=True)
+                self.cursor.execute("select database();")
+                record = self.cursor.fetchone()
+                print("You're connected to database: ", record)
+
+                self.cursor.execute("CREATE DATABASE IF NOT EXISTS niewiadoma DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci")
+                self.cursor.execute("USE niewiadoma")
+                self.cursor.execute("CREATE TABLE IF NOT EXISTS `autor` (`idAutora` int(11) NOT NULL, `nazwaAutora` text NOT NULL, `fotkaAutora` text NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;")
+                self.cursor.execute("CREATE TABLE IF NOT EXISTS `config` (`nazwaPlikuLoga` text NOT NULL, `nazwaStudia` text NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;")
+                self.cursor.execute("CREATE TABLE IF NOT EXISTS `daneadresowe` (`idAdresu` int(11) NOT NULL, `samAdres` text NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;")
+                self.cursor.execute("CREATE TABLE IF NOT EXISTS `demo` (`idOknaWylaczonego` int(11) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;")
+                self.cursor.execute("CREATE TABLE IF NOT EXISTS `gatunek` (`idGatunku` int(11) NOT NULL, `nazwaGatunku` text NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;")
+                self.cursor.execute("CREATE TABLE IF NOT EXISTS `plyta` (`idPlyty` int(11) NOT NULL, `tytulPlyty` text NOT NULL, `dataWydania` datetime NOT NULL, `idGatunku` int(11) NOT NULL, `obrazekOkladki` text NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;")
+                self.cursor.execute("CREATE TABLE IF NOT EXISTS `sprzedaneplyty` (`idParagonu` int(11) NOT NULL, `idUsera` int(11) NOT NULL, `idUtworu` int(11) NOT NULL, `idPlyty` int(11) NOT NULL, `dataSprzedazy` datetime NOT NULL, `idAdresu` int(11) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;")
+                self.cursor.execute("CREATE TABLE IF NOT EXISTS `users` (`idUsera` int(16) NOT NULL, `login` varchar(16) NOT NULL, `pass` varchar(16) NOT NULL, `uprawnienia` int(16) NOT NULL, `avatar` int(16) NOT NULL, `jezykUzytkownika` int(16) NOT NULL, `rodzajSkorkiInterfejsu` int(16) NOT NULL, `portfel` varchar(16) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;")
+                self.cursor.execute("CREATE TABLE IF NOT EXISTS `utwor` (`idUtworu` int(11) NOT NULL, `nazwaUtworu` text NOT NULL, `idAutora` int(11) NOT NULL,`idGatunku` int(11) NOT NULL,`obrazek` text NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;")
 
 
-try:
-    connection = mysql.connector.connect(host='remotemysql.com', #Połącz się z bazą danych
-                                         port='3306',
-                                         database='kkQ248zGAs',
-                                         user='kkQ248zGAs',
-                                         password='3dbum5P0FI')
-    if connection.is_connected(): #Wyświetl informacje o bazie danych
-        db_Info = connection.get_server_info()
-        print("Connected to MySQL Server version ", db_Info)
-        cursor = connection.cursor(buffered=True) #Kursor wykonuje polecenia SQL
-        cursor.execute("select database();")
-        record = cursor.fetchone()
-        print("You're connected to database: ", record)
-                #Interfejs aplikacji
+
+
+        except mysql.connector.Error as e:
+            print("Error while connecting to MySQL", e)
+            self.connection.rollback()
+            self.cursor.close()
+            self.connection.close()
+
+    def close(self):
+        if self.connection.is_connected():
+            self.cursor.close()
+            self.connection.close()
+            print("MySQL connection is closed")
+
+def Register():
+    pass
+
+def Login():
+    pass
+
+class Interface(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
         customtkinter.set_appearance_mode("System")
         customtkinter.set_default_color_theme("dark-blue")
 
-        app = customtkinter.CTk()
-        app.geometry("400x240")
-        mainframe= customtkinter.CTkFrame(app, padx=1, pady=1, fg_color="#ADD8E6")
-        app.configure(fg_color="#ADD8E6")
+        self.geometry("400x240")
+        mainframe= customtkinter.CTkFrame(self, fg_color="#ADD8E6")
         mainframe.grid(column=0, row=0, sticky="nsew")
-        app.columnconfigure(0, weight=1)
-        app.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
 
-        login_textbox = customtkinter.CTkTextbox(app, fg_color="#ADD8E6") #Text proszący o login i hasło
+        login_textbox = customtkinter.CTkTextbox(self, fg_color="#ADD8E6")
         login_textbox.grid(row=0, column=0)
         login_textbox.insert("0.0", "Podaj login i hasło")
         login_textbox.configure(state="disabled")
 
+#        login_errorbox = customtkinter.CTkTextbox(app, fg_color="#ADD8E6")
+#        self.textbox.grid(row=0, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
+#        login_errorbox.grid(row=1, column=1)
+#        login_errorbox.insert("0.0", "Błędny login i hasło, spróbuj ponownie")
+#        login_errorbox.configure(state="disabled")
 
-        login_entry = customtkinter.CTkEntry(master=app, placeholder_text="Login") #Pole login
+
+        login_entry = customtkinter.CTkEntry(master=self, placeholder_text="Login")
         login_entry.place(relx=0.5, rely=0.3, anchor=customtkinter.CENTER)
 
-        password_entry = customtkinter.CTkEntry(master=app, placeholder_text="Hasło", text_color="White") #Pole Hasło
+        password_entry = customtkinter.CTkEntry(master=self, placeholder_text="Hasło", text_color="White")
         password_entry.place(relx=0.5, rely=0.4, anchor=customtkinter.CENTER)
 
-
-        # def Dodaj_przyklad(): #Guzik
-        #         sql = "INSERT INTO autor (idAutora, nazwaAutora, fotkaAutora) VALUES (%s, %s, %s)" #Dodaje nowego autora
-        #         val = (1, "Tentacooler", "http://pokemonmeme.com/cache/70f8b164/ava2b422db4b70224cd3d.png")
-        #         cursor.execute(sql, val)
-        #         connection.commit()
-        #         print(cursor.rowcount, "Record inserted")
-        #
-        # button = customtkinter.CTkButton(master=app, text="Dodaj przykładowe dane do bazy danych", command=Dodaj_przyklad)
-        # button.place(relx=0.5, rely=0.65, anchor=customtkinter.CENTER)
-
-
-        def Login():
-             pass#W tym miejscu ma być funkcja logowania. Pobierz login i hasło z login_entry i password_entry.
-
-        button = customtkinter.CTkButton(master=app, text="Zaloguj", command=Login)
+        button = customtkinter.CTkButton(master=self, text="Zaloguj", command=Login)
         button.place(relx=0.5, rely=0.525, anchor=customtkinter.CENTER)
 
-        def Register():
-             #W tym miejscu ma być funkcja Rejestrowania. Pobierz login i hasło z login_entry i password_entry.
-            login = login_entry.get()
-            password = password_entry.get()
-            
-            cursor.execute("SELECT * FROM users WHERE uprawnienia = \"2\" ")
-            result = cursor.fetchall
-            if cursor.rowcount==0:
-                uprawnienia = "2"
-            else:
-                uprawnienia = "0"
-            sql = "INSERT INTO users (login, pass, uprawnienia, avatar, jezykUzytkownika, rodzajSkorkiInterfejsu, portfel) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-            val = (login, password, uprawnienia, "0", "0", "0", "0")
-            cursor.execute(sql, val)
-            connection.commit()
-            print("Dodano uzytkownika")
-
-        button = customtkinter.CTkButton(master=app, text="Zarejestruj", command=Register)
+        button = customtkinter.CTkButton(master=self, text="Zarejestruj", command=Register)
         button.place(relx=0.5, rely=0.65, anchor=customtkinter.CENTER)
 
-
-        # def usun_przyklad():
-        #         sql = "DELETE FROM autor WHERE idAutora = 1" #Usuwa dane z autora
-        #         cursor.execute(sql)
-        #         connection.commit()
-        #         print(cursor.rowcount, "Records deleted")
-        #
-        # button = customtkinter.CTkButton(master=app, text="Usuń przykładowe dane", command=usun_przyklad)
-        # button.place(relx=0.5, rely=0.775, anchor=customtkinter.CENTER)
+    def spawn_main_window(self):
+        main_window = customtkinter.CTkToplevel(self)
+        main_window.geometry("800x400")
 
 
-        app.mainloop()
-                #Inter
+conn = MySQLConnection('192.166.219.220', '3306', 'niewiadoma', 'niewiadoma', 'Niewiadoma2244;')
+conn.connect()
 
+login_window = Interface()
+login_window.mainloop()
 
-        cursor.execute("SELECT * FROM autor") #Wyświetla zawartość tabeli autor
-        result = cursor.fetchall()
-        for x in result:
-            print(x)
-
-
-
-
-except Error as e: #Jak coś się zepsuje to powiedz dlaczego
-    print("Error while connecting to MySQL", e)
-finally: #Zamknij połączenie na koniec, nawet jak coś się zepsuje
-    if connection.is_connected():
-        cursor.close()
-        connection.close()
-        print("MySQL connection is closed")
+conn.close()
